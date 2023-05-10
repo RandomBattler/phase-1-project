7124f2a4d8849e84eb2cdbc69d3816a9aed6c283
Steve Romero <romero.steve.a@gmail.com>
2023-05-10 15:10:56 -0700

    Changed button click to form submit to add item to order

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//TO DO
+//change click to a form submit for adding order
+//fix order list to take more than just the name
+//set radio buttons for sizes
+//edit order
+//Enter customer name
+//confirm and check out button
+//welcome and closing screens
+
 const orderList = [];
 let totalCost = 0;
 
@@ -6,15 +15,30 @@ let totalCost = 0;
 function addToOrder(item){
     //push item to the list
 
-    orderList.push(item.name);
-    let h5 = document.createElement("h5");
-    h5.textContent = item.name;
-    document.getElementById("order-list").appendChild(h5);
+//    orderList.push(item.name);
+    //let h5 = document.createElement("h5");
+    const orderLi = document.createElement("li");
+    orderLi.textContent = item.name + "    $" + item.cost;
+    const del = document.createElement("button");
+
+    del.addEventListener("click", ()=>{
+        deleteItem(del, item);
+    });
+    del.value = "X";
+    orderLi.appendChild(del);
+    document.getElementById("order-list").appendChild(orderLi);
     //add customize and remove buttons
+    
     totalCost += item.cost;
 
 
     document.getElementById("total-cost").innerText = "$"+totalCost.toFixed(2);
+}
+function deleteItem(del, item){
+    totalCost -= item.cost;
+    document.getElementById("total-cost").innerText = "$"+totalCost.toFixed(2);
+    del.parentNode.remove();
+    
 }
 
 //populate the menu display
@@ -22,29 +46,37 @@ function addMenuItem(item){
     let it = document.createElement("div");
 
 
-    let h2 = document.createElement("h2");
+    let h2 = document.createElement("h3");
     h2.textContent = item.name;
-    let h3 = document.createElement("h3");
+    let h3 = document.createElement("dt");
     h3.textContent = item.description;
-    let h4 = document.createElement("h4");
+    let h4 = document.createElement("dd");
     h4.textContent = "$"+item.cost;
 
+    let form = document.createElement("form");
+    form.id = "form-"+item.name;
+
     let button = document.createElement("button");
-    button.classList.add("add-btn");
+    button.type = "submit"
     button.id = item.id;
     button.textContent = "Add";
 
-    button.addEventListener("click", () =>{
+    form.addEventListener("submit", (e) =>{
+        e.preventDefault();
         addToOrder(item);
+     //   console.log(e);
     });
 
-    it.append(h2,h3,h4,button);
+    form.appendChild(button);
+
+    h2.append(h3,h4);
+    it.append(h2,form);
     document.getElementById("menu-list").appendChild(it);
 
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://localhost:3000/array").then(response => response.json())
+    fetch("http://localhost:3000/menu").then(response => response.json())
     .then(items => items.forEach(item=> addMenuItem(item)));
 })
